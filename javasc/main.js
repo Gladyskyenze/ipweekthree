55e8d2b6d66fb8003631b4d3298fc2409a8e3801
Gladyskyenze <kyenzegladys@gmail.com>
2022-03-22 13:49:45 +0300

    making code follow smooth

--- javasc/main.js
+++ javasc/main.js
@@ -1,12 +1,12 @@
-$('#user_form').submit(function(){
-let namee  = $('#name').val();
-let emaill=$('#email').val();
-let msg= $('#messagee').val();
-alert(namee + 'We have received your message. Thank you for the feedback.');
+$('#user_form').submit(function () {
+    let namee = $('#name').val();
+    let emaill = $('#email').val();
+    let msg = $('#messagee').val();
+    alert(namee + 'We have received your message. Thank you for the feedback.');
 
 });
-$(document).ready(function(){
-    $(#w4).hover(function(){
-        $(this).css("background-color","white");
-  });
-});
+// $(document).ready(function () {
+//     $(#w4).hover(function () {
+//         $(this).css("background-color", "white");
+//     });
+// });
