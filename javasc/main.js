b4b21f65e73f3036c3c67b71f38fb27adf835b89
Gladyskyenze <kyenzegladys@gmail.com>
2022-03-22 13:38:32 +0300

    trying jquery hover

--- javasc/main.js
+++ javasc/main.js
@@ -5,3 +5,8 @@ let msg= $('#messagee').val();
 alert(namee + 'We have received your message. Thank you for the feedback.');
 
 });
+$(document).ready(function(){
+    $(#w4).hover(function(){
+        $(this).css("background-color","white");
+  });
+});
